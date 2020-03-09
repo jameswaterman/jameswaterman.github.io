683a51fa1d16481c24d2b653651709c4048c20b3
James Waterman <jameswaterman@aol.co.uk>
2020-03-09 01:07:37 +0000

    Add show more/less toggle to blog.

--- resources/js/blog-js.js
+++ resources/js/blog-js.js
@@ -1,3 +1,5 @@
+// Dropdown menu
+
 $(document).ready(function() {
 	$('.dropdown').click(function() {
 		$('.dropdown-content').slideToggle(300);
@@ -5,5 +7,15 @@ $(document).ready(function() {
 		$('span.top').toggleClass('bar1');
 		$('span.middle').toggleClass('bar2');
 		$('span.bottom').toggleClass('bar3');
+	});
+});
+
+// show more/less toggle
+
+$(document).ready(function() {
+	$('.show-button').click(function() {
+		$('.more-content').slideToggle(300);
+		$('.elipsis').slideToggle(300);
+	    $('span.show-button').toggleClass('show-more').toggleClass('show-less');
 	});
 });
