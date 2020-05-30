0d716bb937775848e2c47ccd2374cceeb95cb24e
James Waterman <jameswaterman@aol.co.uk>
2020-05-30 17:26:52 +0100

    Landing page/code updates.

--- resources/js/blog-js.js
+++ resources/js/blog-js.js
@@ -19,3 +19,70 @@ $(document).ready(function() {
 	    $('span.show-button').toggleClass('show-more').toggleClass('show-less');
 	});
 });
+
+// throttle for parallax
+
+const throttle = (func, limit) => {
+
+    const background = document.querySelector('.parallax');
+
+	if (background === null) {
+	    let lastFunc
+	    let lastRan
+	    return function() {
+	        const context = this
+	        const args = arguments
+	        if (!lastRan) {
+	            func.apply(context, args)
+	            lastRan = Date.now()
+	        } else {
+	            clearTimeout(lastFunc)
+	            lastFunc = setTimeout(function() {
+	                if ((Date.now() - lastRan) >= limit) {
+	                    func.apply(context, args)
+	                    lastRan = Date.now()
+	                }
+	            }, limit - (Date.now() - lastRan))
+	        };
+	    };
+	} else {
+		return func;
+	};
+};
+
+// Parallax effect
+
+const parallaxEffect = (e) => {
+
+	let windowHeight = window.innerHeight * 1.2;
+    let scrolled = window.pageYOffset;
+	const background = document.querySelector('.parallax');
+
+	if (window.innerHeight <= 250) {
+    	windowHeight = window.innerHeight * 5;
+    } else if (window.innerHeight <= 500) {
+    	windowHeight = window.innerHeight * 4;
+    }
+
+    if (background === null || scrolled > windowHeight) {
+    	return;
+    };
+
+   	background.style.top = (scrolled * 0.25) + 'px';
+};
+	
+
+
+window.addEventListener('scroll', throttle(parallaxEffect, 10000));
+
+
+
+
+
+
+
+
+
+
+
+
